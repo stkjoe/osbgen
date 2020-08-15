9eadc7069a8e601d6d3322a7f43ef9f7cb7bcd97
stkjoe <28199363+stkjoe@users.noreply.github.com>
2020-08-15 21:30:30 +1200

    Make string-building modular with base class

--- osbgen/event.py
+++ osbgen/event.py
@@ -75,173 +75,114 @@ class Event:
         self.easing = easing
         self.startTime = startTime
         self.endTime = endTime
+        self.startValue = ""
+        self.endValue = ""
+        self.eventType = ""
 
         # module-specific parameters.
+
+    def getEndValue(self):
+        return self.endValue if self.endValue != "" else self.startValue
+
+    def getEndTime(self):
+        return self.endTime if self.endTime != "" else self.startTime
     
     def compile(self, writer):
-        writer.write(str(self))
+        writer.write(self.getLine())
+        current = {}
+        current[self.__class__.__name__]["Value"] = self.getEndValue()
+        current[self.__class__.__name__]["Time"] = self.getEndTime()
+        current["Time"] = self.getEndTime()
+        return current
+
+    def getLine(self):
+        return ' {},{},{},{},{}{}\n'.format(self.eventType, self.easing, self.startTime,
+                                            self.endTime, ",".join(self.startValue),
+                                            ",{}".format(",".join(self.endValue)) if not (
+                                                         self.startValue == self.endValue or
+                                                         self.endValue == "")
+                                                         else "")
+
 
 # Represents a fade command.
 class Fade(Event):
     def __init__(self, startTime, startFade, endTime, endFade, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startFade = startFade
-        self.endFade = endFade
-
-    def __str__(self):
-        return ' F,{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                           self.startFade,
-                                           # If endFade is the same as startFade
-                                           # Then it's fine to omit the last parameter.
-                                           ",{}".format(self.endFade) if not (
-                                                        self.endFade == self.startFade) 
-                                                        else "")
+        self.startValue = startFade
+        self.endValue = endFade
+        self.eventType = "F"
 
 # Represents a movementX command.
 class MoveX(Event):
     def __init__(self, startTime, startX, endTime, endX, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startX = startX
-        self.endX = endX
-
-    def __str__(self):
-        return ' MX,{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                            self.startX,
-                                            # If endX is the same as startX,
-                                            # Then it's fine to omit the last parameter.
-                                            ",{}".format(self.endX) if not (
-                                                         self.endX == self.startX or
-                                                         self.endX == "")
-                                                         else "")
+        self.startValue = startX
+        self.endValue = endX
+        self.eventType = "MX"
 
 # Represents a movementY command.
 class MoveY(Event):
     def __init__(self, startTime, startY, endTime, endY, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startY = startY
-        self.endY = endY
-
-    def __str__(self):
-        return ' MY,{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                            self.startY,
-                                            # If endY is the same as startY,
-                                            # Then it's fine to omit the last parameter.
-                                            ",{}".format(self.endY) if not (
-                                                         self.endY == self.startY or
-                                                         self.endY == "")
-                                                         else "")
+        self.startValue = startY
+        self.endValue = endY
+        self.eventType = "MY"
 
 # Represents a movement command.
 class Move(Event):
     def __init__(self, startTime, startX, startY, endTime, endX, endY, easing):
         super().__init__(startTime, endTime, easing)
-        self.startX = startX
-        self.endX = endX
-        self.startY = startY
-        self.endY = endY
-
-    def __str__(self):
-        return ' M,{},{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                              self.startX, self.startY, 
-                                              # If endX and endY are the same as startX and startY,
-                                              # Then it's fine to omit the last two parameters.
-                                              ",{},{}".format(self.endX, self.endY) if not (
-                                                              self.endX == self.startX and 
-                                                              self.endY == self.startY) 
-                                                              else "")
+        self.startValue = [startX, startY]
+        self.endValue = [endX, endY]
+        self.eventType = "M"
 
 # Represents a scale command.
 class Scale(Event):
     def __init__(self, startTime, startScale, endTime, endScale, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startScale = startScale
-        self.endScale = endScale
-
-    def __str__(self):
-        return ' S,{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                           self.startScale,
-                                           # If endScale is the same as startScale
-                                           # Then it's fine to omit the last parameter.
-                                           ",{}".format(self.endScale) if not (
-                                                        self.endScale == self.startScale or
-                                                        self.endScale == "") 
-                                                        else "")
+        self.startValue = startScale
+        self.endValue = endScale
+        self.eventType = "S"
 
 # Represents a vector command.
 class Vector(Event):
     def __init__(self, startTime, startX, startY, endTime, endX, endY, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startX = startX
-        self.startY = startY
-        self.endX = endX
-        self.endY = endY
-
-    def __str__(self):
-        return ' V,{},{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                              self.startX, self.startY,
-                                              # If endVector is the same as startVector
-                                              # Then it's fine to omit the last parameter.
-                                              ",{},{}".format(self.endX, self.endY) if not (
-                                                              self.endX == self.startX and 
-                                                              self.endY == self.startY) 
-                                                              else "")
+        self.startValue = [startX, startY]
+        self.endValue = [endX, endY]
+        self.eventType = "V"
 
 # Represents a rotate command.
 class Rotate(Event):
     def __init__(self, startTime, startRotate, endTime, endRotate, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startRotate = startRotate
-        self.endRotate = endRotate
-
-    def __str__(self):
-        return ' R,{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                           self.startRotate,
-                                           # If endRotate is the same as startRotate
-                                           # Then it's fine to omit the last parameter.
-                                           ",{}".format(self.endRotate) if not (
-                                                        self.endRotate == self.startRotate) 
-                                                        else "")
+        self.startValue = startRotate
+        self.endValue = endRotate
+        self.eventType = "R"
 
 # Represents a colour command.
 class Colour(Event):
     def __init__(self, startTime, startR, startG, startB, endTime, endR, endG, endB, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.startR = startR
-        self.startG = startG
-        self.startB = startB
-        self.endR = endR
-        self.endG = endG
-        self.endB = endB
-
-    def __str__(self):
-        return ' C,{},{},{},{},{},{}{}\n'.format(self.easing, self.startTime, self.endTime,
-                                                 self.startR, self.startG, self.startB,
-                                                 # If endColour is the same as startColour
-                                                 # Then it's fine to omit the last parameter.
-                                                 ",{},{},{}".format(self.endR, self.endG, self.endB) if not ((
-                                                                    self.startR == self.endR and
-                                                                    self.startG == self.endG and
-                                                                    self.startB == self.endB) or (
-                                                                    self.endR + self.endG + self.endB == ""))
-                                                                    else "")
+        self.startValue = [startR, startG, startB]
+        self.endValue = [endR, endG, endB]
+        self.eventType = "C"
 
 # Represents an other parameter command.
 class Parameter(Event):
     def __init__(self, startTime, endTime, param, easing):
         super().__init__(startTime, endTime, easing)
         # osu!-specific parameters.
-        self.param = param
-
-    def __str__(self):
-        return ' P,{},{},{},{}\n'.format(self.easing, self.startTime, self.endTime,
-                                         self.param)
+        self.startValue = param
+        # Setting endValue to param so the string builder will ignore.
+        self.endValue = param
 
 # Represents a Loop command.
 class Loop(Layer):
@@ -256,8 +197,9 @@ class Loop(Layer):
         for event in self.events:
             writer.write(" ")
             event.compile(writer)
+        return {}
 
-    def __str__(self):
+    def getLine(self):
         return ' L,{},{}\n'.format(self.startTime, self.loopCount)
 
 # Represents a Trigger command.
@@ -274,6 +216,7 @@ class Trigger(Layer):
         for event in self.events:
             writer.write(" ")
             event.compile(writer)
+        return {}
 
-    def __str__(self):
+    def getLine(self):
         return ' T,{},{},{}\n'.format(self.triggerName, self.start, self.end)
