425e385b7fcb919c5f47cadb1230644239e1ebb1
stkjoe <28199363+stkjoe@users.noreply.github.com>
2020-08-19 21:23:52 +1200

    Add license and add more details to setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,21 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="osbgen",
-    version="0.0.1",
+    version="1.0.0.dev1",
     author="stkjoe",
     author_email="joestephen1997@gmail.com",
+    description="A python library to create osu! storyboards in an object-orientated way.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/stkjoe/osbgen",
+    license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: tbc :: tbc",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
     ],
     python_requires=">=3.8",
 )
